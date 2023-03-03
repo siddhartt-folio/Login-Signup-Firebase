fff2ff662554682d2d62af815931fa6548b80138
Sarup Das <neeopixel@gmail.com>
2023-03-03 20:04:16 +0530

    Helmet App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './images/logo.svg';
 import './App.css';
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+                <meta charSet="utf-8" />
+                <title>React-template</title>
+                <describe>Create React Firebase App with this template</describe>
+      </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
